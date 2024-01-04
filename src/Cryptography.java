00c85c7edc5d9f2b0d2d6c43b9d1ada879cd345f
Akande Al-ameen <longlasting1805@gmail.com>
2024-01-04 16:45:37 +0100

    encrypted

--- src/Cryptography.java
+++ src/Cryptography.java
@@ -4,13 +4,22 @@ public class Cryptography {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int numberCounter = 1;
+        System.out.print("Enter four numbers: ");
+        int originalNumber = scanner.nextInt();
 
-        while (numberCounter <= 4){
-            System.out.print("Enter number: ");
-            int number = scanner.nextInt();
-            numberCounter+=1;
+        // encrypt the original number
+        int encryptedNumber = encrypt(originalNumber);
 
-        }
+        // Display the encrypted number
+        System.out.println("Encrypted number: " + encryptedNumber);
+
+    }
+    public static int encrypt(int number){
+        int digit1 = (number % 1000 / 100 + 7) % 10;
+        int digit2 = (number % 100 / 10 + 7) % 10;
+        int digit3 = (number % 10 + 7) % 10;
+        int digit4 = (number % 1000 + 7) % 10;
+
+        return digit3 * 1000 + digit4 * 100 + digit1 * 10 + digit2;
     }
 }
