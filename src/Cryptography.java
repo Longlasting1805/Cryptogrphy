eb02d774fdc23ce0d3a4f7ded3f39fc3dd37eb96
Akande Al-ameen <longlasting1805@gmail.com>
2024-01-04 10:33:20 +0100

    first commit

---
+++ src/Cryptography.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Cryptography {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberCounter = 1;
+
+        while (numberCounter <= 4){
+            System.out.print("Enter number: ");
+            int number = scanner.nextInt();
+            numberCounter+=1;
+
+        }
+    }
+}
